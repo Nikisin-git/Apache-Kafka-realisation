044833ad1ff8e89e34ad0e31012e3efe5de5e06e
Nick Evil <57915192+Nikisin-git@users.noreply.github.com>
2025-05-31 06:11:11 +0300

    Add files via upload

---
+++ HomeWork5 — копия/route_service/app/main.py
@@ -0,0 +1,25 @@
+from contextlib import asynccontextmanager
+
+import uvicorn
+from fastapi import FastAPI
+
+from app.db import ensure_indexes
+from app.routers import route_router
+from app.routers.route_router import repo
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    await repo.start()
+    await ensure_indexes()
+    yield
+    await repo.stop()
+    
+app = FastAPI(
+    title="Route Service",
+    lifespan=lifespan,
+)
+app.include_router(route_router.router)
+
+
+if __name__ == "__main__":
+    uvicorn.run("main:main_app", host="0.0.0.0", port=8000, reload=True)
---
+++ HomeWork5 — копия/route_service/app/repositories/route_repository.py
@@ -0,0 +1,92 @@
+import json
+import os
+from typing import List, Optional
+
+from aiokafka import AIOKafkaProducer
+from bson import ObjectId
+
+
+from app.models import Route
+from app.db import db
+
+KAFKA_TOPIC = "routes-topic"
+BOOTSTRAP_SERVERS = os.getenv("KAFKA_BOOTSTRAP_SERVERS")
+
+class RouteRepository:
+    collection = db.routes
+
+    def __init__(self):
+        self.producer = AIOKafkaProducer(bootstrap_servers=BOOTSTRAP_SERVERS)
+
+    async def start(self):
+        await self.producer.start()
+
+    async def stop(self):
+        await self.producer.stop()
+
+    async def create_route(self, start: str, end: str, waypoints: list[str], distance_length: str) -> Route:
+        route_id = str(ObjectId())
+        event = {
+            "type": "route_created",
+            "payload": {
+                "route_id": route_id,
+                "start_point": start,
+                "end_point": end,
+                "waypoints": waypoints,
+                "distance": distance_length
+            }
+        }
+        await self.producer.send_and_wait(KAFKA_TOPIC, json.dumps(event).encode())
+        return Route(route_id=route_id, start_point=start, end_point=end, waypoints=waypoints, distance= distance_length)
+         
+    @staticmethod
+    async def get_route_by_id(route_id: str) -> Optional[Route]:
+        doc = await RouteRepository.collection.find_one({"_id": ObjectId(route_id)})
+        if not doc:
+            return None
+        return Route(
+            route_id=str(doc["_id"]),
+            start_point=doc["start_point"],
+            end_point=doc["end_point"],
+            waypoints=doc.get("waypoints", []),
+            distance=int(doc["distance"])
+        )
+
+    @staticmethod
+    async def get_all_routes() -> List[Route]:
+        routes: List[Route] = []
+        async for doc in RouteRepository.collection.find():
+            routes.append(
+                Route(
+                    route_id=str(doc["_id"]),
+                    start_point=doc["start_point"],
+                    end_point=doc["end_point"],
+                    waypoints=doc.get("waypoints", []),
+                    distance=int(doc["distance"])
+                )
+            )
+        return routes
+    
+    @staticmethod
+    async def find_routes(
+        start_point: Optional[str] = None,
+        end_point: Optional[str] = None,
+    ) -> List[Route]:
+        query: dict = {}
+        if start_point:
+            query["start_point"] = start_point
+        if end_point:
+            query["end_point"] = end_point
+
+        routes: List[Route] = []
+        async for doc in RouteRepository.collection.find(query):
+            routes.append(
+                Route(
+                    route_id=str(doc["_id"]),
+                    start_point=doc["start_point"],
+                    end_point=doc["end_point"],
+                    waypoints=doc.get("waypoints", []),
+                    distance=int(doc["distance"])
+                )
+            )
+        return routes
